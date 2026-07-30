aa7774732eb3e21af95d3767e15131555dea1d55
Илья Владиславович <vol@MacBook-Air-Dzeff.local>
2026-07-30 18:47:21 +0300

    Connect finance dashboard to Google Sheets

---
+++ data/secretary-config.js
@@ -0,0 +1,16 @@
+window.LIFE_OS_SECRETARY_CONFIG = {
+  snapshotUrl: "",
+  headers: {},
+  finance: {
+    tabs: {
+      summary: "",
+      operations: "https://docs.google.com/spreadsheets/d/15BsyIn4eglomCan8By63l1RV-3bTAY8Hkcy1xuyInLY/gviz/tq?tqx=out:csv&gid=1009",
+      primaIncome: "https://docs.google.com/spreadsheets/d/15BsyIn4eglomCan8By63l1RV-3bTAY8Hkcy1xuyInLY/gviz/tq?tqx=out:csv&gid=20260713&range=A14:Q1000&headers=1",
+      accounts: "",
+      payments: "",
+      debts: "",
+      goals: ""
+    },
+    headers: {}
+  }
+};
