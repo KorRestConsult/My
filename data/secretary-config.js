5f31902aa0ac61bfef12c6899a36ff3918348567
Илья Владиславович <vol@MacBook-Air-Dzeff.local>
2026-07-30 19:31:25 +0300

    Improve finance sheet dashboard

--- data/secretary-config.js
+++ data/secretary-config.js
@@ -2,6 +2,8 @@ window.LIFE_OS_SECRETARY_CONFIG = {
   snapshotUrl: "",
   headers: {},
   finance: {
+    spreadsheetId: "15BsyIn4eglomCan8By63l1RV-3bTAY8Hkcy1xuyInLY",
+    autoRefreshMs: 300000,
     tabs: {
       summary: "",
       operations: "https://docs.google.com/spreadsheets/d/15BsyIn4eglomCan8By63l1RV-3bTAY8Hkcy1xuyInLY/gviz/tq?tqx=out:csv&gid=1009",
