24bbf8de5d39c958fd60e8f3627f094b3e96dfa7
Илья Владиславович <vol@MacBook-Air-Dzeff.local>
2026-07-30 19:08:21 +0300

    Clarify finance blocks and colors

--- data/secretary-config.js
+++ data/secretary-config.js
@@ -5,7 +5,6 @@ window.LIFE_OS_SECRETARY_CONFIG = {
     tabs: {
       summary: "",
       operations: "https://docs.google.com/spreadsheets/d/15BsyIn4eglomCan8By63l1RV-3bTAY8Hkcy1xuyInLY/gviz/tq?tqx=out:csv&gid=1009",
-      primaIncome: "https://docs.google.com/spreadsheets/d/15BsyIn4eglomCan8By63l1RV-3bTAY8Hkcy1xuyInLY/gviz/tq?tqx=out:csv&gid=20260713&range=A14:Q1000&headers=1",
       accounts: "",
       payments: "",
       debts: "",
